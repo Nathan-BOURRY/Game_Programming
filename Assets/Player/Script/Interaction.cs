0ebb707611012ec46b2b2b602e2a4dcb2c8025b0
VincentTJW <v.terentjew@hotmail.fr>
2023-03-03 16:42:51 +0100

    MovePlayer

--- Assets/Player/Script/Interaction.cs
+++ Assets/Player/Script/Interaction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class Interaction : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Player/Script/player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player : MonoBehaviour
+{
+    static player instance = null;
+
+    private CharacterController controller = null;
+    public float speed = 10.0f;
+
+    Vector2 movement = Vector2.zero;
+
+    Vector2 velocity = Vector2.zero;
+
+    SpriteRenderer spr = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+        spr = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity.x = movement.x*speed;
+        velocity.y = movement.y*speed;
+      
+       
+       
+        if(movement.x != 0){
+            spr.flipX = movement.x < 0;
+        }
+        
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnMove(InputValue val){
+        movement = val.Get<Vector2>();
+    }
+ 
+}
