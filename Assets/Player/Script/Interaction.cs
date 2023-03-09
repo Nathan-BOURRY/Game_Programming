cccf929de52c07e89b205307615f83c7d71629d2
VincentTJW <v.terentjew@hotmail.fr>
2023-03-09 15:54:54 +0100

    Interaction

--- Assets/Player/Script/Interaction.cs
+++ Assets/Player/Script/Interaction.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Interaction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public string levelToLoad = "";
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name != "Player"){
+            return;
+        }
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
