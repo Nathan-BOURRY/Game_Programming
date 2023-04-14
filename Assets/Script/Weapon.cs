c6539f02d5c469013adf2f8c9236462d70951237
Nathan-BOURRY <nathan.bourry@gmail.com>
2023-04-14 21:52:57 +0200

    Input_Manager

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -41,43 +41,48 @@ public class Weapon : MonoBehaviour
         // {
         //     Debug.Log("YES");
         // }
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        //if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        //{
+
+        //}
+
+    }
+    public void OnFire()
+    {
+        if (player.numberOfBullet > 0)
         {
-            if (player.numberOfBullet > 0)
+            audio.Play();
+            player.numberOfBullet = player.numberOfBullet - 1;
+            rotation = Mathf.Rad2Deg * Mathf.Atan2(-playerMovement.x, playerMovement.y) + 90;
+
+            //TODO : Fix Spawn bullet with animation
+
+            if (rotation >= 45 && rotation <= 135) // En haut
             {
-                audio.Play();
-                player.numberOfBullet = player.numberOfBullet - 1;
-                rotation = Mathf.Rad2Deg * Mathf.Atan2(-playerMovement.x, playerMovement.y) + 90;
-
-                //TODO : Fix Spawn bullet with animation
-
-                if (rotation >= 45 && rotation <= 135) // En haut
-                {
-                    instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x, Spawn.position.y + 0.5f), Quaternion.Euler(0, 0, rotation));
-                }
-                else if (rotation > 135 && rotation <= 225) //A gauche
-                {
-                    instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x - 1.5f, Spawn.position.y), Quaternion.Euler(0, 0, rotation));
-                }
-                else if (rotation > 225 && rotation < 315)
-                {
-                    instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x, Spawn.position.y - 0.5f), Quaternion.Euler(0, 0, rotation));
-                }
-                else //Droite
-                {
-                    instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x + 0.6f, Spawn.position.y), Quaternion.Euler(0, 0, rotation));
-                }
-                instanBullet.GetComponent<Bullet>().mouvement = playerMovement;
-
+                instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x, Spawn.position.y + 0.5f), Quaternion.Euler(0, 0, rotation));
             }
-            else
+            else if (rotation > 135 && rotation <= 225) //A gauche
             {
-                //todo : text no bullet to show on hud
-                //Debug.Log("plus de balle");
-                /*if(audio2.isPlaying == false){
-                    audio2.Play();
-                }*/
+                instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x - 1.5f, Spawn.position.y), Quaternion.Euler(0, 0, rotation));
             }
+            else if (rotation > 225 && rotation < 315)
+            {
+                instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x, Spawn.position.y - 0.5f), Quaternion.Euler(0, 0, rotation));
+            }
+            else //Droite
+            {
+                instanBullet = Instantiate(Bullet, new Vector2(Spawn.position.x + 0.6f, Spawn.position.y), Quaternion.Euler(0, 0, rotation));
+            }
+            instanBullet.GetComponent<Bullet>().mouvement = playerMovement;
+
+        }
+        else
+        {
+            //todo : text no bullet to show on hud
+            //Debug.Log("plus de balle");
+            /*if(audio2.isPlaying == false){
+                audio2.Play();
+            }*/
         }
     }
 }
